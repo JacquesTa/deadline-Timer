5f1aafcb2380383032d1910feff9d9c50297cbc0
Jacques Mouafo Tagu <jacques_fabrice32@yahoo.fr>
2021-01-23 13:59:49 +0100

    added messages to the code

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+"use strict";
+
+// defining the variables
 const daysEl = document.getElementById("days");
 const hoursEl = document.getElementById("hours");
 const minutesEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
 
+//functions: To convert time(milliseconds) into date
+//Function to update the clock
+// Function to format the time(adding a zero in front of a single number)
 function convertToDays(milliSeconds) {
   let days = Math.floor(milliSeconds / (86400 * 1000));
   milliSeconds -= days * (86400 * 1000);
@@ -54,3 +60,55 @@ document.getElementById("form-c").addEventListener("keyup", function (event) {
     document.getElementById("subBtn").click();
   }
 });
+////////////////////////////////////////////////////////////////////
+
+//To fix: Saving the date entered after the click so that when write the new date it only changes the InnerHtml after the click on submit.
+
+//next step saving the state of the page to the url to be able to send it------ Brainstorming
+function save_new_state() {
+  // a temp data object to serialize
+  var data = {
+    txt: text_input.value,
+    num: number_input.value,
+  };
+  // serialize the data and save behind the url's #
+  window.location.hash = serialize(data);
+  // show the current url in a readonly text field
+  url_input.value = window.location.toString();
+}
+
+// every time the page loads, see if some state came with it
+function load_state() {
+  // will hold data if there is any
+  var data;
+  // get the current url hash (behind the #)
+  var url_hash = window.location.hash.substr(1);
+  console.log("hash on page load is", url_hash); // debug in dev
+  // check for and apply state if any is present
+  if (url_hash !== undefined && url_hash !== "") {
+    console.log("whoa"); // we found shit
+    // try turning the hash string into data
+    data = deserialize(url_hash);
+    if (data === undefined) {
+      return; // nope, nothing here
+    }
+    // do we see what we'd expect?
+    if (data.txt !== undefined) {
+      text_input.value = data.txt;
+    }
+    if (data.num !== undefined) {
+      number_input.value = data.num;
+    }
+  }
+  // update the url text field for dummies
+  url_input.value = window.location.toString();
+}
+
+// on window load, try to load state
+window.onload = load_state;
+////////////////////////////////////////////////////////////////////////////////////////
+if (window.location.hash) {
+  // Fragment exists
+} else {
+  // Fragment doesn't exist
+}
